elenadimitrova/argent-contracts-1

Change assert.revert to assertRevert Remove verboseWaitForTransaction and use tx receipt from the transaction object Update method parameters to abiEncode to not be array

test/baseWallet.js
@@ -190,7 +190,7 @@ contract("BaseWallet", (accounts) => {
       await oldWallet.init(owner, [module1.address]);
       await module1.callDapp(oldWallet.address);
       await module1.callDapp2(oldWallet.address, 2, false);
-      await assert.revert(module1.fail(oldWallet.address, "just because"));
+      await assertRevert(module1.fail(oldWallet.address, "just because"));
     });
   });
 
@@ -200,7 +200,7 @@ contract("BaseWallet", (accounts) => {
       await oldWallet.init(owner, [module1.address]);
       await module1.callDapp(oldWallet.address);
       await module1.callDapp2(oldWallet.address, 2, true);
-      await assert.revert(module1.fail(oldWallet.address, "just because"));
+      await assertRevert(module1.fail(oldWallet.address, "just because"));
     });
   });
 });

mrheyday/argent-contracts

test/baseWallet.js
@@ -211,7 +211,7 @@ contract("BaseWallet", (accounts) => {
       await module1.upgradeWallet(oldWallet.address, await module1.lastVersion(), { from: owner });
       await feature1.callDapp(oldWallet.address);
       await feature1.callDapp2(oldWallet.address, 2, false);
-      await assert.revert(feature1.fail(oldWallet.address, "just because"));
+      await assertRevert(feature1.fail(oldWallet.address, "just because"));
     });
   });
 
@@ -222,7 +222,7 @@ contract("BaseWallet", (accounts) => {
       await module1.upgradeWallet(oldWallet.address, await module1.lastVersion(), { from: owner });
       await feature1.callDapp(oldWallet.address);
       await feature1.callDapp2(oldWallet.address, 2, true);
-      await assert.revert(feature1.fail(oldWallet.address, "just because"));
+      await assertRevert(feature1.fail(oldWallet.address, "just because"));
     });
   });
 });